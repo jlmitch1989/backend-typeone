209b9d96712d3e63f734cab9b94c2b719faf51a9
jlmitch1989 <joshuamitchell@Joshuas-MacBook-Pro-2.local>
2016-09-29 17:25:15 -0400

    Fix comments route
    Nested comments route within posts route so that
    I was able to successfully post comments on posts.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  resources :posts, except: [:new, :edit] do
+    resources :comments, only: [:index, :create]
+  end
   resources :comments, except: [:new, :edit]
-  resources :posts, except: [:new, :edit]
   resources :examples, except: [:new, :edit]
   post '/sign-up' => 'users#signup'
   post '/sign-in' => 'users#signin'
