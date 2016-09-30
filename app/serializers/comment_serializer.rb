96c97e09c818a63e6a29a018dbf88ab225848ebd
jlmitch1989 <joshuamitchell@Joshuas-MacBook-Pro-2.local>
2016-09-30 11:59:41 -0400

    Fix current user button bug
    Used editable boolean in comment and post serializer
    to show buttons for current user posts and comments
    and hide other user buttons.

--- app/serializers/comment_serializer.rb
+++ app/serializers/comment_serializer.rb
@@ -1,7 +1,11 @@
 class CommentSerializer < ActiveModel::Serializer
-  attributes :id, :body, :post
+  attributes :id, :body, :post, :editable
   has_one :user
   has_one :post
+
+  def editable
+    scope == object.user
+  end
 
   def post
     object.post.id
--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,6 +1,11 @@
 class PostSerializer < ActiveModel::Serializer
-  attributes :id, :title, :content, :comments
+  attributes :id, :title, :content, :comments, :user_id, :editable
   has_one :user
+
+  # So that you can hide buttons for non current-users
+  def editable
+    scope == object.user
+  end
 
   def comments
     object.comments.pluck(:id)
